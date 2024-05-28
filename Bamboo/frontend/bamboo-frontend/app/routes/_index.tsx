6e1f09fa6945d0fff482a1db72f29ae9c782543b
Tsien Jin <tsien.jin@gmail.com>
2024-05-28 08:26:33 +0800

    Testing tailwindcss classes

--- Bamboo/frontend/bamboo-frontend/app/routes/_index.tsx
+++ Bamboo/frontend/bamboo-frontend/app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className={`text-5xl font-black`}>Welcome to Remix</h1>
       <ul>
         <li>
           <a
