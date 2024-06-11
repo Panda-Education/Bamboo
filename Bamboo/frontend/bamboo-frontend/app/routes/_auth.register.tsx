ba538dbab6a2020f44ad94574a900266ac3fdda0
Tsien Jin <tsien.jin@gmail.com>
2024-06-11 17:03:20 +0800

    Created register route file

---
+++ Bamboo/frontend/bamboo-frontend/app/routes/_auth.register.tsx
@@ -0,0 +1,11 @@
+import GradientLayout from '@/components/layouts/gradient-layout';
+
+
+export default function RegisterRoute(){
+
+  return(
+    <GradientLayout>
+      Register
+    </GradientLayout>
+  )
+}
