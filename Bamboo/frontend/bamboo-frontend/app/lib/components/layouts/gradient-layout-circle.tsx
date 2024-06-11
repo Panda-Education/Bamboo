71014d15f4ab8a60c20df4c1c83958b2a6fdcd06
Tsien Jin <tsien.jin@gmail.com>
2024-06-11 17:02:53 +0800

    Created gradient layout

---
+++ Bamboo/frontend/bamboo-frontend/app/lib/components/layouts/gradient-layout-circle.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/utils';
+
+
+export default function GradientLayoutCircle(
+  {
+    className=""
+  }:{
+    className?:string
+  }
+){
+
+
+  return(
+    <div className={cn(`
+    rounded-full absolute 
+    w-[100px] h-[100px]
+    bg-slate-500
+    top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+    `, className)}/>
+  )
+
+}
