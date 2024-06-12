6634fbab1a5ed527ebf6df948edeb93593c02153
Tsien Jin <tsien.jin@gmail.com>
2024-06-13 00:08:16 +0800

    Updated app module to include these routes

--- Bamboo/backend/bamboo-backend/src/app.module.ts
+++ Bamboo/backend/bamboo-backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { RouterModule } from '@nestjs/core';
+import { RegisterModule } from './auth/register/register.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    RegisterModule,
+    RouterModule.register([
+      {
+        path: 'auth',
+        module: AuthModule,
+        children: [
+          {
+            path: "register",
+            module: RegisterModule
+          }
+        ]
+      }
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
