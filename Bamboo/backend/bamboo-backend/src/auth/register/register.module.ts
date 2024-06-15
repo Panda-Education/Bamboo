fc481bcdde6324e941de9e2f45699f17a454752a
Tsien Jin <tsien.jin@gmail.com>
2024-06-15 19:33:33 +0800

    Added boilerplate endpoint for login

--- Bamboo/backend/bamboo-backend/src/auth/register/register.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RegisterController } from './register.controller';
-
-@Module({
-  controllers: [RegisterController]
-})
-export class RegisterModule {}
