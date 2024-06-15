d67cafc802de15a832db95892451245a4fa32d2a
Tsien Jin <tsien.jin@gmail.com>
2024-06-15 19:25:50 +0800

    Added global pipe to validate DTOs

--- Bamboo/backend/bamboo-backend/src/main.ts
+++ Bamboo/backend/bamboo-backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Strips properties that do not have decorators
+    forbidNonWhitelisted: true, // Throws an error if non-whitelisted properties are present
+    transform: true, // Transforms payloads to be objects typed according to their DTO classes
+  }));
   await app.listen(3000);
 }
 bootstrap().catch(console.error)
