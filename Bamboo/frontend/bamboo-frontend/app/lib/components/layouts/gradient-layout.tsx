e1b0441112b714cd7bb5c5534ee1c252526e1c02
Tsien Jin <tsien.jin@gmail.com>
2024-06-15 19:37:13 +0800

    Improved visual texture of gradient layout

--- Bamboo/frontend/bamboo-frontend/app/lib/components/layouts/gradient-layout.tsx
+++ Bamboo/frontend/bamboo-frontend/app/lib/components/layouts/gradient-layout.tsx
@@ -39,19 +39,19 @@ export default function GradientLayout(
       <div className={`
       hidden md:block
       md:relative flex-grow
-      blur-3xl opacity-100
+      blur-[100px] opacity-100
       -z-50
       w-full md:min-h-[100dvh]
       `}>
         <GradientLayoutCircle className={`top-[50%] left-[50%] w-[500px] h-[500px] bg-slate-50`} />
-        <GradientLayoutCircle className={`top-[25%] left-[25%] w-[400px] h-[400px] bg-slate-200`} />
-        <GradientLayoutCircle className={`top-[70%] left-[50%] w-[450px] h-[450px] bg-slate-300`} />
-        <GradientLayoutCircle className={`top-[55%] left-[80%] w-[450px] h-[450px] bg-slate-300`} />
-        <GradientLayoutCircle className={`top-[10%] left-[10%] w-[400px] h-[400px] bg-slate-200`} />
+        <GradientLayoutCircle className={`top-[25%] left-[25%] w-[400px] h-[400px] bg-slate-400`} />
+        <GradientLayoutCircle className={`top-[70%] left-[50%] w-[450px] h-[450px] bg-slate-400`} />
+        <GradientLayoutCircle className={`top-[55%] left-[80%] w-[450px] h-[450px] bg-slate-400`} />
+        <GradientLayoutCircle className={`top-[10%] left-[10%] w-[400px] h-[400px] bg-slate-300`} />
         <GradientLayoutCircle className={`top-[30%] left-[90%] w-[350px] h-[350px] bg-slate-200`} />
-        <GradientLayoutCircle className={`top-[30%] left-[90%] w-[350px] h-[350px] bg-slate-200`} />
-        <GradientLayoutCircle className={`top-[90%] left-[10%] w-[200px] h-[200px] bg-slate-200`} />
-        <GradientLayoutCircle className={`top-[70%] left-[20%] w-[400px] h-[400px] bg-slate-100`} />
+        <GradientLayoutCircle className={`top-[30%] left-[90%] w-[350px] h-[350px] bg-slate-300`} />
+        <GradientLayoutCircle className={`top-[90%] left-[10%] w-[200px] h-[200px] bg-slate-300`} />
+        <GradientLayoutCircle className={`top-[70%] left-[20%] w-[400px] h-[400px] bg-slate-200`} />
         <GradientLayoutCircle className={`top-[50%] left-[50%] w-[80%] h-[80%] bg-slate-50 opacity-30`} />
       </div>
 
