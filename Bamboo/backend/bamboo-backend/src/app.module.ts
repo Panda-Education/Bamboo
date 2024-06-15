86f776a6fcdb257582b0a95a7357df22b5e0efd1
Tsien Jin <tsien.jin@gmail.com>
2024-06-15 19:34:32 +0800

    Imports and all to app module

--- Bamboo/backend/bamboo-backend/src/app.module.ts
+++ Bamboo/backend/bamboo-backend/src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './routes/auth/auth.module';
 import { RouterModule } from '@nestjs/core';
-import { RegisterModule } from './auth/register/register.module';
+import { RegisterModule } from './routes/auth/register/register.module';
+import { NestjsFormDataModule } from 'nestjs-form-data';
+import { UserService } from './services/user/user.service';
+import { DbPrismaService } from './services/db/db-prisma/db-prisma.service';
+import { configDotenv } from 'dotenv';
+import { PasswordService } from './services/password/password.service';
+import * as process from 'node:process';
 
+
+
+//// Load environment variables
+configDotenv({
+  path: `./.env.${process.env.NODE_ENV || 'local'}`
+})
 
 @Module({
   imports: [
+    NestjsFormDataModule,
     AuthModule,
     RegisterModule,
     RouterModule.register([
@@ -24,6 +37,6 @@ import { RegisterModule } from './auth/register/register.module';
     ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, DbPrismaService, PasswordService],
 })
 export class AppModule {}
