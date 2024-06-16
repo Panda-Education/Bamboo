b66c168935f8b2cf5b545be1ed37abc3a090623c
Tsien Jin <tsien.jin@gmail.com>
2024-06-16 15:45:48 +0800

    Added first and last name to DTO

--- Bamboo/backend/bamboo-backend/src/dto/auth/register-user-from-email-password/register-user-from-email-password.ts
+++ Bamboo/backend/bamboo-backend/src/dto/auth/register-user-from-email-password/register-user-from-email-password.ts
@@ -1,7 +1,19 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, isNotEmpty, IsNotEmpty, IsString, Matches } from 'class-validator';
 import { Expose } from 'class-transformer';
 
 export class RegisterUserFromEmailPassword {
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^[a-zA-Z\s]+$/)
+  @Expose()
+  readonly firstName: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^[a-zA-Z\s]+$/)
+  @Expose()
+  readonly lastName: string
 
   @IsNotEmpty()
   @IsString()
