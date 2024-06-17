b4484489ad835b2b56be71de20a2924813277e9f
Tsien Jin <tsien.jin@gmail.com>
2024-06-17 23:25:09 +0800

    Added REMIX URL to CORS backend

--- Bamboo/backend/bamboo-backend/src/main.ts
+++ Bamboo/backend/bamboo-backend/src/main.ts
@@ -8,7 +8,14 @@ async function bootstrap() {
     whitelist: true, // Strips properties that do not have decorators
     forbidNonWhitelisted: true, // Throws an error if non-whitelisted properties are present
     transform: true, // Transforms payloads to be objects typed according to their DTO classes
-  }));
+  }))
+  app.enableCors({
+    origin: [
+      'http://localhost:5173'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
   await app.listen(3000);
 }
 bootstrap().catch(console.error)
