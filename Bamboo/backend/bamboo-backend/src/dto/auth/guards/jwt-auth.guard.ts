fc56bae4580e3f00af9788f6b46abc380c0418d3
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2024-06-20 00:42:09 +0800

    strategies and guards for google

---
+++ Bamboo/backend/bamboo-backend/src/dto/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Injectable()
+export class JwtGuard extends AuthGuard("jwt") {}
---
+++ Bamboo/backend/bamboo-backend/src/dto/auth/strategies/jwt-auth.strategy.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+
+@Injectable()
+export class JwtGuardStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
+
+    async validate(payload: any){
+        console.log('payload', payload)
+        return payload;
+    }
+}
